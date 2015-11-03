e409cc048c8ed2d3d81acd488fbdc89805e84c06
ikkedus <matvandergragt@gmail.com>
2015-11-03 12:20:55 -0800

    test

---
+++ Chats/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Chats.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+        public void Sendmessage(string name, string message) {
+            //update alle clients
+            Clients.All.addNewMessageToPage(name, message);
+        }
+    }
+}
